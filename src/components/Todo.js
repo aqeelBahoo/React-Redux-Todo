08d0c140f6b9a5a0f0eb2d5fa205d8607f56dd2e
Aqueel Bahoo <mr.aqeelbahoo@gmail.com>
2020-09-13 20:54:51 +0500

    -Added Advance todo application icon

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import AddTodo from './AddTodo';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import AddTodo from "./AddTodo";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core/styles";
 import TodoList from "./TodoList";
 import Filters from "./Filters";
 
 const useStyles = makeStyles(() => ({
-    todoContainer: {
-        margin: '20px'
-    }
-}))
+  todoContainer: {
+    margin: "20px",
+  },
+  title: {
+    textAlign: "center",
+  },
+}));
 const Todo = () => {
-    const classes = useStyles();
-    return <Card className={classes.todoContainer} >
-        <CardContent>
-            <Filters />
-            <AddTodo />
-            <TodoList />
-        </CardContent>
-        <CardActions>
-        </CardActions>
+  const classes = useStyles();
+  return (
+    <Card className={classes.todoContainer}>
+      <CardContent>
+        <h1 className={classes.title}>Advance Todo</h1>
+        <Filters />
+        <AddTodo />
+        <TodoList />
+      </CardContent>
+      <CardActions></CardActions>
     </Card>
-}
+  );
+};
 export default Todo;
